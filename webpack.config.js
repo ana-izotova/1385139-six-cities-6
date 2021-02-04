550b559b376174a0be284bff3a22630fffe3229b
Anastasia <ana.izotova@gmail.com>
2021-02-04 19:38:56 +0300

    setup environment

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    open: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'source-map',
+};
