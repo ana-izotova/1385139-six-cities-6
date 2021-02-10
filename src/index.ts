661511cf92c53a49cb038a18d3942172662eb75b
Anastasia <ana.izotova@gmail.com>
2021-02-10 18:57:24 +0300

    refactor adapter

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/app/app";
 import {OfferCard} from "./types";
+import {adaptToClient} from "./utils";
 
 fetch(`https://6.react.pages.academy/six-cities/hotels`)
   .then((response) => response.json())
   .then((response) => {
-    const cards = response.map((card) => <OfferCard>{
-      ...card,
-      isFavorite: card.is_favorite,
-      isPremium: card.is_premium,
-      maxAdults: card.max_adults,
-      host: {
-        ...card.host,
-        avatarUrl: card.host.avatar_url,
-        isPro: card.host.is_pro,
-      },
-    });
+    const cards = response.map((card) => adaptToClient(card));
 
     ReactDom.render(
         React.createElement(App, {cards}),
